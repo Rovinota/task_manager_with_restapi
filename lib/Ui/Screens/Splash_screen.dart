0d59724590c5b1f1bc6006bca4dcfd3f1e13fa28
Rovinota <tanvir.cse8.bu@gmail.com>
2024-04-23 04:43:51 +0600

    Initial commit

---
+++ lib/Ui/Screens/Splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Splash_Screens extends StatefulWidget {
+  const Splash_Screens({super.key});
+
+  @override
+  State<Splash_Screens> createState() => _Splash_ScreensState();
+}
+
+class _Splash_ScreensState extends State<Splash_Screens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
